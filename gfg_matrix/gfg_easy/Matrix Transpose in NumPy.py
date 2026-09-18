d96b3350bfa3bb69ee1438e5053e0fa4cc2d4d51
harshita1362 <147728364+harshita1362@users.noreply.github.com>
2026-09-18 15:14:46 +0530

    Create Matrix Transpose in NumPy.py

---
+++ gfg_matrix/gfg_easy/Matrix Transpose in NumPy.py
@@ -0,0 +1,6 @@
+# Matrix Transpose in NumPy
+# Given a 2-dimensional NumPy array arr, find and return its transpose
+import numpy as np
+class Solution:
+    def transposeMatrix(self, arr):
+        return arr.T
