1ddfb92a6c136660a9f99992d4ae2602a1226428
harshita1362 <147728364+harshita1362@users.noreply.github.com>
2026-09-18 15:11:21 +0530

    Create Create a 3D Array from a List of Lists.py

---
+++ gfg_matrix/gfg_easy/Create a 3D Array from a List of Lists.py
@@ -0,0 +1,6 @@
+# Create a 3D Array from a List of Lists
+# Given a list of lists representing a three-dimensional structure data, create and return a 3D NumPy array from it.
+import numpy as np
+class Solution:
+    def create3DArray(self, data):
+        return np.array(data)
