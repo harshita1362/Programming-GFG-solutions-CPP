4464221294bdae30226150bf00ab379dbb3474f5
harshita1362 <147728364+harshita1362@users.noreply.github.com>
2026-09-18 15:18:07 +0530

    Create Diagonal in a 2D Numpy Array.cpp

---
+++ gfg_matrix/gfg_easy/Diagonal in a 2D Numpy Array.cpp
@@ -0,0 +1,6 @@
+# Diagonal in a 2D Numpy Array
+# Given a 2D NumPy array arr, extract and return its diagonal elements.
+
+class Solution:
+    def diagonalElements(self, arr):
+        return arr.diagonal()
