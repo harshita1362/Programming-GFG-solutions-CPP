5505aa93271360f2ec3289ff816046adbe8b23e6
harshita1362 <147728364+harshita1362@users.noreply.github.com>
2026-09-23 16:35:40 +0530

    Update Kth Bit Value.cpp

--- gfg_bit_magic/gfg_basic/Kth Bit Value.cpp
+++ gfg_bit_magic/gfg_basic/Kth Bit Value.cpp
@@ -9,3 +9,7 @@ class Solution {
 		return (n>> (k - 1)) & 1;
 	}
 };
+/*
+n >> (k - 1) shifts the bits of n right by k-1 positions, bringing the kth bit to the rightmost position.
+& 1 extracts that rightmost bit, giving either 0 or 1.
+*/
